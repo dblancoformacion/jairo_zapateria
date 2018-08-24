a579c318c77b3f043a5c0658b843867912664e49
unknown <jairo.setienpeyrac@gmail.com>
2018-08-24 11:07:24 +0200

    cambio de extension del index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Zap-In Shoe Company</title>
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="1.css">
+	<link href="https://fonts.googleapis.com/css?family=Poiret+One|Roboto+Mono" rel="stylesheet">
+	
+</head>
+<body>
+	<div class="wrapper">
+	<div class="header">
+		<h1>Zap-In Shoe Company</h1>
+	</div>
+	<div class="content">
+	<div class="container-fluid">
+
+	</div>
+	<div class="container-fluid">
+
+	</div>
+	</div>
+	<div class="footer">
+		<p>Trabajo realizado por Jairo Setién Peyrac</p>
+	</div>
+
+
+
+	</div>
+	
+</body>
+</html>
